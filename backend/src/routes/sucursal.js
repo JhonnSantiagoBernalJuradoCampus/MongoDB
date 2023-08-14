643b77b7d37337a01f942cc1b0dc33c6fa43ffa6
JhonnSantiagoBernalJuradoCampus <jhonbernalcampus@gmail.com>
2023-08-13 21:23:41 -0500

    feat: :construction: Crud sucursal en proces, get, post

--- backend/src/routes/sucursal.js
+++ backend/src/routes/sucursal.js
@@ -28,8 +28,51 @@ router.get("/automovil", limit(), async (req,res)=>{
         res.send(automovil);
     } catch (error) {
         console.error(error);
-        res.status(500).send("Error en el servidor");
+        res.status(500).send({message: "Error en el servidor"});
     }
 });
+
+//crud
+router.get("/", limit(), async (req,res)=>{
+    if(!req.rateLimit) return;
+    try {
+        const db = await connectionDB();
+        const sucursal = db.collection("sucursales")
+        
+        const result = await sucursal.find().toArray();
+        res.send(result);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send({message: "Error en el servidor"});
+    }
+});
+router.post("/", limit(), async (req,res)=>{
+    /**
+     * @var {req.body}
+     * req.body =
+        {
+            "_id": 6,
+            "ID_sucursal": 3,
+            "Nombre": "Cabecera",
+            "Direccion": "United states",
+            "Telefono": 123124124
+        }
+     */
+    if(!req.rateLimit) return;
+    try {
+        const db = await connectionDB();
+        const sucursal = db.collection("sucursales");
+        
+        const nuevo = await sucursal.insertOne(req.body);
+        res.status(201).send({message: "Creado correctamente"})
+    } catch (error) {
+        const err = error.errInfo.details.schemaRulesNotSatisfied[0];
+        (err.missingProperties)
+        ? res.status(417).send({message: `El campo ${err.missingProperties[0]} es obligatorio`})
+        : (err.propertiesNotSatisfied)
+        ? res.status(417).send({message: err.propertiesNotSatisfied[0].description})
+        : res.status(500).send({message: error})
+    }
+})
 
 export default router;
